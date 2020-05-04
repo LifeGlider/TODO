0e35bf9fd52b57c4801840f15443788d17b53e75
LifeGlider <b.panchenko@sloboda-studio.com>
2020-05-04 22:20:13 +0300

    remove 'arrow' from model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates_with TaskValidator
   validates :todo, presence: true
-  validates_length_of :todo, :maximum => 300
+  validates_length_of :todo, maximum: 300
   validates_inclusion_of :done, :important, in: [true, false]
 end
