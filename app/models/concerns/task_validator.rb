c6b55b3a3f24287519119c0decc8c976988bd036
LifeGlider <b.panchenko@sloboda-studio.com>
2020-05-04 22:14:13 +0300

    replace russian letter in method

--- app/models/concerns/task_validator.rb
+++ app/models/concerns/task_validator.rb
@@ -1,7 +1,7 @@
 class TaskValidator < ActiveModel::Validator
   def validate(record)
     if Task.where("lower(replace(todo, ' ', '')) = lower(replace(?, ' ', ''))", record.todo).exists?
-      record.errors.add(:todo, "ыuch task already exists")
+      record.errors.add(:todo, "such task already exists")
     end
   end
 end
